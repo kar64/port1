b7643c7f9e3f2ba85a097315b4b76f1dfb0dc23a
kar64 <140837613+kar64@users.noreply.github.com>
2023-10-16 07:55:13 +0400

    Add files via upload

---
+++ port1/js/script.js
@@ -0,0 +1,25 @@
+
+    $(".hamburger").click(function(){
+        $(this).toggleClass("is-active");
+    
+        if($(this).hasClass('is-active')){
+            console.log("down");
+            $('.header_wraper_menu').slideDown(300);
+        }else{
+            console.log("up");
+            $('.header_wraper_menu').slideUp(300);
+        }
+    });
+    
+    
+    console.log("zdorova zaebal");
+    $('.review_container_row_item').click(function(){
+        const revID=$(this).data('revid');
+        console.log(revID);
+        $('.review_container_row_item').not(this).removeClass('active');
+        $(this).addClass('active');
+        $('.review_container_var').not(revID).removeClass('active');
+        $(revID).addClass('active');
+    });
+
+
